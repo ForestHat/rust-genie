e4cb01724a8ed776d1222bcdbe04149a53d8d0c4
ForestHat <146117189+ForestHat@users.noreply.github.com>
2024-03-07 05:11:09 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,48 @@
 use actix_web::{get, App, HttpServer, Result as AwResult};
 use maud::{html, Markup};
+use serde::{Deserialize, Serialize};
 use std::io;
-use select::document::Document;
-use select::predicate::{Class, Name, Predicate};
-use std::process::Command;
-use std::thread;
 
-static mut GL_NEWS_VEC: Vec<String> = Vec::new();
-static mut GL_THEME_VEC: Vec<String> = Vec::new();
-static mut GL_URL_VEC: Vec<String> = Vec::new();
+#[derive(Serialize, Deserialize)]
+struct News {
+    #[serde(rename = "Id")]
+    id: u16,
+    #[serde(rename = "Title")]
+    title: String,
+    #[serde(rename = "Url")]
+    url: String,
+    #[serde(rename = "Theme")]
+    theme: String
+}
 
 #[get("/")]
 async fn index() -> AwResult<Markup> {
-    unsafe {
-        if GL_NEWS_VEC.len() != 0 {
-            GL_NEWS_VEC.clear();
-            GL_THEME_VEC.clear();
-            GL_URL_VEC.clear();
-        }
-    }
-    test().await;
+    let news_feed: Vec<News> = get_news().await;
     
     Ok(
-        unsafe {
-            html! {
-                html {
-                    body {
-                        div style="text-align: center;" {
-                            header {
-                                h1 {
-                                    "News site!"
-                                }
+        html! {
+            html {
+                body {
+                    div style="text-align: center;" {
+                        header {
+                            h1 {
+                                "News site!"
+                            }
                             }
                             h2 {
                                 "Personolized news"
                             }
                             div {
-                                @for i in 0..GL_NEWS_VEC.len() {
-                                    a href={(GL_URL_VEC.get(i).unwrap())} {
-                                        (GL_NEWS_VEC.get(i).unwrap().to_owned() + " THEME: " + GL_THEME_VEC.get(i).unwrap())
+                                @for i in 0..news_feed.len() {
+                                    a href={(news_feed.get(i).unwrap().url)} {
+                                        (news_feed.get(i).unwrap().title.to_owned() + &String::from(" THEME: ") + &news_feed.get(i).unwrap().theme)
                                         br;
                                     }
                                 }
                             }
-                            footer {
-                                p {
-                                    "ForestHat"
-                                }
+                        footer {
+                            p {
+                                "ForestHat"
                             }
                         }
                     }
@@ -58,57 +54,17 @@ async fn index() -> AwResult<Markup> {
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    println!("Started!");
     HttpServer::new(|| {
         App::new().service(index)
     }).bind(("127.0.0.1", 8080))?.run().await
 }
 
+async fn get_news() -> Vec<News> {
+    let client = reqwest::Client::new();
+    let request = client.get("http://localhost:4040");
+    let resp = request.send().await.expect("Error while parsing!");
+    let json = resp.text().await.expect("Error get the json!");
 
-async fn test() {
-    let resp = reqwest::get("https://habr.com/ru/news/").await.unwrap();
-    let doc = Document::from(resp.text().await.unwrap().as_str());
-
-    let mut news_vector: Vec<String> = Vec::new();
-    let mut url_vector: Vec<String> = Vec::new();
-    
-    let mut size: usize = 0;
-
-    for node in doc.find(Class("tm-articles-list__item")) {
-        let news: String = node.find(Class("tm-title__link").descendant(Name("span"))).next().unwrap().text();
-        news_vector.push(news);
-
-        let url: String = node.find(Class("tm-title__link")).next().unwrap().attr("href").unwrap().to_string();
-        url_vector.push(String::from("https://habr.com") + &url);
-
-        size += 1;
-    }
-
-    let mut handles = Vec::new();
-
-    for i in 0..size {
-        let news: String = news_vector.get(i).unwrap().to_string();
-        let url: String = url_vector.get(i).unwrap().to_string();
-
-        let handle = thread::spawn(move || {
-            run_cmd(news, url);
-        });
-
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-}
-
-fn run_cmd(news: String, url: String) {
-    let exec: String = String::from("python3");
-    let output = Command::new(exec).args(&["main.py", &news]).output().unwrap();
-    let out = String::from_utf8_lossy(&output.stdout).replace("\n", "");
-
-    unsafe {
-        GL_NEWS_VEC.push(news);
-        GL_THEME_VEC.push(out);
-        GL_URL_VEC.push(url);
-    }
+    serde_json::from_str::<Vec<News>>(&json).expect("Error while processing the news data")
 }
